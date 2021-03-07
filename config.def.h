205588add7a7050cac9c27696a3004b454d09c11
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2021-03-08 03:12:07 +0800

    Add openclipboard config

--- config.def.h
+++ config.def.h
@@ -222,6 +222,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ MODKEY,               XK_o,           opencopied,     {.v = "xdg-open"} },
 };
 
 /*
