1fb924cb2dec6890eaefdd3bb4bbe2fb4c8829c4
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2021-04-10 18:52:57 +0800

    Change cursorstyle to block not blinking

--- config.def.h
+++ config.def.h
@@ -164,7 +164,7 @@ static unsigned int defaultrcs = 8;
  * 7: Blinking st cursor
  * 8: Steady st cursor
  */
-static unsigned int cursorstyle = 1;
+static unsigned int cursorstyle = 2;
 static Rune stcursor = 0x2603; /* snowman (U+2603) */
 
 /*
